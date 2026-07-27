d1ceac2b760810dd684858073287a6da9583ebfa
lucasmartineza <lucasmartinez.a@gmail.com>
2026-07-27 20:33:45 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,13 @@
-const CACHE = 'tapir-v6';
-
+const CACHE = 'tapir-v38';
 self.addEventListener('install', e => { self.skipWaiting(); });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
   if(e.request.url.includes('index.html') || e.request.mode === 'navigate'){
     e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
     return;
   }
-  e.respondWith(
-    fetch(e.request).then(response => {
-      if(response && response.status === 200){
-        const clone = response.clone();
-        caches.open(CACHE).then(cache => cache.put(e.request, clone));
-      }
-      return response;
-    }).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
