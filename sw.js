95aa2e4c72a48fb974f90730de2063cf4208d56a
lucasmartineza <lucasmartinez.a@gmail.com>
2026-07-28 15:35:46 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tapir-v411';
+const CACHE = 'tapir-v412';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
